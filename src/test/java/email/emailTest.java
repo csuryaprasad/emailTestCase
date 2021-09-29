9a06b6413ca506c4f945f065a60a716d2da695e9
csuryaprasad <csuryaprasad@gmail.com>
2021-09-29 11:46:46 -0400

    ScreenSHots added

--- src/test/java/email/emailTest.java
+++ src/test/java/email/emailTest.java
@@ -1,15 +1,21 @@
 package email;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.io.FileHandler;
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -37,7 +43,7 @@ public class emailTest {
 	 @Test
 	    public void userLogin()
 	    {
-	        WebElement usernameTxt = driver.findElement(By.id("username"));
+	        WebElement usernameTxt = driver.findElement(By.id("username1"));
 	        usernameTxt.sendKeys("tomsmith");
 	        WebElement passwordTxt = driver.findElement(By.id("password"));
 	        passwordTxt.sendKeys("SuperSecretPassword!");
@@ -46,12 +52,28 @@ public class emailTest {
 	        log.info("Current URL is:" + driver.getCurrentUrl());
 	        Assert.assertTrue(driver.getCurrentUrl().contains("secure"));
 	    }
-
+	 
+	    @AfterMethod
+	    public void tearDown(ITestResult result)   {
+	     
+	    	if(ITestResult.FAILURE==result.getStatus())   	{
+	    		try {
+    				TakesScreenshot ts=(TakesScreenshot)driver;
+    				File source=ts.getScreenshotAs(OutputType.FILE);
+ 					FileHandler.copy(source, new File("./test-output/"+result.getName()+".png"));
+ 					log.info("Screenshot taken");
+	    			} catch (Exception e)	{
+	    				log.info("Exception while taking screenshot "+e.getMessage());
+	    		} 
+	    	}
+	    }
 	    @AfterClass
 	    public void tearDown(){
 	        if (driver != null) {
 	            driver.quit();
 	        }
 	    }
+	    
+
 	 
 }
